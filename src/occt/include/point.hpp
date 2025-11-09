1e5dae9b7bdf1eaa2a472a2085590a1982c42a37
Erick Ahmed <engineering@erickahmed.com>
2025-11-09 18:50:18 +0100

    ffi: remove extern "C"; planning to use cxx for FFI

--- src/occt/include/point.hpp
+++ src/occt/include/point.hpp
@@ -2,7 +2,5 @@
 
 #include <gp_Pnt.hxx>
 
-extern "C" {
-    TopoDS_Shape make_point(double x, double y, double z);
-    void coord_point(const TopoDS_Shape& shape, double* x, double* y, double* z);
-}
+TopoDS_Shape make_point(double x, double y, double z);
+void coord_point(const TopoDS_Shape& shape, double* x, double* y, double* z);
